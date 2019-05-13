8a5c86cbc8d04c1fd33a2457621ce3635805f53d
Ulisses de Souza Melo <ulisses.usm@gmail.com>
2019-05-13 20:14:27 -0300

    crud

---
+++ TodoWebApi/Context/TodoContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoWebApi.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TodoWebApi.Context
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
